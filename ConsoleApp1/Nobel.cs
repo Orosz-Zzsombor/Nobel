022afc1e7cf5fa430c9d6a2d27a59110b1c8e777
Tomi <20.szabo.tamas@mechwart.com>
2023-06-01 10:57:40 +0200

    Végleges

---
+++ ConsoleApp1/Nobel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Nobel
+    {
+        int ev;
+        string tipus;
+        string keresztNev;
+        string vezetekNev;
+
+        public Nobel(string CSVsor)
+        {
+            var mezo = CSVsor.Split(';');
+            this.ev = int.Parse(mezo[0]);
+            this.tipus = mezo[1];
+            this.vezetekNev = mezo[2];
+            this.keresztNev = mezo[3];
+        }
+        public Nobel(int ev, string tipus, string keresztNev, string vezetekNev)
+        {
+            this.ev = ev;
+            this.tipus = tipus;
+            this.keresztNev = keresztNev;
+            this.vezetekNev = vezetekNev;
+        }
+
+        public int Ev { get => ev; set => ev = value; }
+        public string Tipus { get => tipus; set => tipus = value; }
+        public string KeresztNev { get => keresztNev; set => keresztNev = value; }
+        public string VezetekNev { get => vezetekNev; set => vezetekNev = value; }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            List<Nobel> Nobel = new List<Nobel>();
+            File.ReadAllLines("nobel.csv").Skip(1).ToList().ForEach(x => Nobel.Add(new Nobel(x)));
+            Nobel dijas =Nobel.Find(x => x.Ev == 2017 && x.Tipus == "irodalmi");
+            Console.WriteLine($"{dijas.VezetekNev} {dijas.KeresztNev}");
+            Console.WriteLine("2. feladat: ");
+            Nobel.Where(x => x.VezetekNev.Contains("Curie")).GroupBy(x => x.KeresztNev).ToList().ForEach(x => Console.WriteLine($" {x.Key}"));
+            Nobel.GroupBy(x => x.Tipus).ToList().ForEach(x => Console.WriteLine($"{x.Key}  {x.Count()}db"));
+            StreamWriter sw = new StreamWriter("orvosi.txt", true);
+            Nobel.Where(x => x.Tipus == "orvosi").ToList().ForEach(x => sw.WriteLine(x))
         }
     }
 }
